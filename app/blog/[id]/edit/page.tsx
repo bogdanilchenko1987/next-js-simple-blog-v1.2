54feba3e1fb974f1f7d5b297f3a4e4435b8ba222
bogdanilchenko1987 <bogdan_ilchenko@ukr.net>
2024-12-08 04:01:38 +0200

    555

--- app/blog/[id]/edit/page.tsx
+++ app/blog/[id]/edit/page.tsx
@@ -1,42 +1,33 @@
 import { getPostsById } from "@/services/getPosts";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
-const BASE_URL = "https://673dfa890118dbfe8609a017.mockapi.io";
+import { Metadata } from "next";
+import { updatePost } from "../../actions";
+import Link from "next/link";
+
 type Props = {
-  params: Promise<{
-    id: string;
-  }>;
+  params: Promise<{ id: string }>;
 };
 
-async function updatePost(data: FormData) {
-  "use server";
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const post = await getPostsById(id);
 
-  const { title, body, id } = Object.fromEntries(data);
-
-  const response = await fetch(`${BASE_URL}/posts/${id}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title, body }),
-  });
-
-  const post = await response.json();
-
-  revalidatePath(`/blog/${post.id}`);
-  redirect(`/blog/${post.id}`);
+  return {
+    title: `Profile of: ${post.title}`,
+  };
 }
 
-export default async function Profile(props: Props) {
-  const params = await props.params;
-
-  const { id } = params;
-
+export default async function Profile({ params }: Props) {
+  const { id } = await params;
   const post = await getPostsById(id);
 
   return (
     <div>
-      <h1>Profile of {post.title}</h1>
+      <h2>
+        Profile of: <i>{post.title}</i>
+      </h2>
+      <Link href={`/blog/${post.id}`}>Back to post</Link>
+      <br />
+      <br />
 
       <form className="form" action={updatePost}>
         <input
@@ -51,6 +42,8 @@ export default async function Profile(props: Props) {
           required
           name="body"
           defaultValue={post.body}
+          rows={5}
+          cols={5}
         />
         <input type="hidden" name="id" value={post.id} />
         <div>
@@ -62,35 +55,44 @@ export default async function Profile(props: Props) {
 }
 
 // import { getPostsById } from "@/services/getPosts";
-// import { Metadata } from "next";
-// import { updatePost } from "../../actions";
-// import Link from "next/link";
-
+// import { revalidatePath } from "next/cache";
+// import { redirect } from "next/navigation";
+// const BASE_URL = "https://673dfa890118dbfe8609a017.mockapi.io";
 // type Props = {
-//   params: Promise<{ id: string }>;
+//   params: Promise<{
+//     id: string;
+//   }>;
 // };
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   const { id } = await params;
-//   const post = await getPostsById(id);
+// async function updatePost(data: FormData) {
+//   "use server";
 
-//   return {
-//     title: `Profile of: ${post.title}`,
-//   };
+//   const { title, body, id } = Object.fromEntries(data);
+
+//   const response = await fetch(`${BASE_URL}/posts/${id}`, {
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ title, body }),
+//   });
+
+//   const post = await response.json();
+
+//   revalidatePath(`/blog/${post.id}`);
+//   redirect(`/blog/${post.id}`);
 // }
 
-// export default async function Profile({ params }: Props) {
-//   const { id } = await params;
+// export default async function Profile(props: Props) {
+//   const params = await props.params;
+
+//   const { id } = params;
+
 //   const post = await getPostsById(id);
 
 //   return (
 //     <div>
-//       <h2>
-//         Profile of: <i>{post.title}</i>
-//       </h2>
-//       <Link href={`/blog/${post.id}`}>Back to post</Link>
-//       <br />
-//       <br />
+//       <h1>Profile of {post.title}</h1>
 
 //       <form className="form" action={updatePost}>
 //         <input
@@ -105,8 +107,6 @@ export default async function Profile(props: Props) {
 //           required
 //           name="body"
 //           defaultValue={post.body}
-//           rows={5}
-//           cols={5}
 //         />
 //         <input type="hidden" name="id" value={post.id} />
 //         <div>
